d6c914322fdecc49085b31e1533ef6851319a373
Sergio Bajo <sergio.bajo@xeridia.com>
2025-05-19 13:53:09 +0200

    Try deployments

--- src/main/java/com/sergio/service/DeploymentServiceImpl.java
+++ src/main/java/com/sergio/service/DeploymentServiceImpl.java
@@ -1,8 +1,10 @@
 package com.sergio.service;
 
 import com.pulumi.Context;
+import com.pulumi.automation.AutomationException;
 import com.pulumi.automation.ConfigValue;
 import com.pulumi.automation.LocalWorkspace;
+import com.pulumi.automation.WorkspaceStack;
 import com.pulumi.core.Output;
 import com.pulumi.kubernetes.apps.v1.Deployment;
 import com.pulumi.kubernetes.apps.v1.DeploymentArgs;
@@ -15,34 +17,54 @@ import com.pulumi.kubernetes.meta.v1.inputs.LabelSelectorArgs;
 import com.pulumi.kubernetes.meta.v1.inputs.ObjectMetaArgs;
 import com.sergio.dto.UpCommand;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Service
 @Slf4j
-public class DeploymentServiceImpl implements DeploymentService{
+public class DeploymentServiceImpl implements DeploymentService {
+
+    private String projectName;
+
+    private String stackName;
+
+    private WorkspaceStack stack;
+
+    public DeploymentServiceImpl(@Value("${pulumi.projectName}") String projectName, @Value("${pulumi.stackName}") String stackName) {
+        this.projectName = projectName;
+        this.stackName = stackName;
+
+        try {
+            stack = LocalWorkspace.createOrSelectStack(projectName, stackName, this::pulumiProgram);
+        } catch (Exception e) {
+            log.error("Error while connecting to Pulumi stack", e);
+        }
+
+    }
+
     @Override
     public void up(UpCommand upCommand) {
-        var projectName = "PulumiJavaTest";
-        var stackName = "dev";
-        try (var stack = LocalWorkspace.createOrSelectStack(projectName, stackName, this::pulumiProgram)) {
+        try {
+            log.info("Updating Pulumi stack to work with {} pods", upCommand.getNumberOfPods());
             stack.setConfig("numberOfPods", new ConfigValue(String.valueOf(upCommand.getNumberOfPods()), false));
             stack.setConfig("isMinikube", new ConfigValue(Boolean.TRUE.toString(), false));
             stack.up();
-        } catch (Exception e) {
-            log.error("Error while performing update", e);
+            log.info("Pulumi stack updated");
+        } catch (AutomationException e) {
+            log.error("Error while updating Pulumi stack");
         }
     }
 
     @Override
     public void destroy() {
-        var projectName = "PulumiJavaTest";
-        var stackName = "dev";
-        try (var stack = LocalWorkspace.createOrSelectStack(projectName, stackName, this::pulumiProgram)) {
+        try {
+            log.info("Destroying Pulumi stack");
             stack.destroy();
-        } catch (Exception e) {
-            log.error("Error while performing update", e);
+            log.info("Pulumi stack destroyed");
+        } catch (AutomationException e) {
+            log.error("Error while destroying Pulumi stack");
         }
     }
 
